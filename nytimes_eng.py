7701244c21270ed3c34783ca1cad28065ae75935
ckoyuncu <59180341+ckoyuncu@users.noreply.github.com>
2021-05-21 15:43:19 +0200

    Update nytimes_eng.py

--- nytimes_eng.py
+++ nytimes_eng.py
@@ -1,3 +1,4 @@
+import requests.exceptions
 from pynytimes import NYTAPI
 from bs4 import BeautifulSoup
 import urllib
@@ -9,75 +10,74 @@ API_KEY = '1HXamLhbfLrsj4BUYmKZUGnZV2QLwAtR'
 
 nyt = NYTAPI(API_KEY, https=False)
 
-# top_stories = nyt.top_stories()
-#
-# # Get all the top stories from a specific category
-# top_science_stories = nyt.top_stories(section = "science")
-
-# data = nyt.archive_metadata(
-#     date = datetime.datetime(2019, 10, 1)
-# )
 
 def content(url):
+    """
+    Uses web crawling to acquire the content through the URLs obtained by the NYT API.
+    """
     try:
-        result = ''
-        site = urllib.request.urlopen(url)
+        result = ''  
+        site = urllib.request.urlopen(url)  
         content = site.read()
-        soup = BeautifulSoup(content, 'lxml')
-        table = soup.findAll('p')
-        for line in table:
-            result += line.text
+        soup = BeautifulSoup(content, 'lxml')  
+        table = soup.findAll('p')  
+        for line in table: 
+            result += line.text  
             result += ' '
         return result
     except:
         return ''
-    
 
 
 def search(query, begin, end):
-    a, b, c = begin.split(',')
-    x, y, z = end.split(',')
-    articles = nyt.article_search(
-        query = query,
-        results = 99,
-        dates = {
-            "begin": datetime.datetime(int(a), int(b), int(c)),
-            "end": datetime.datetime(int(x), int(y), int(z))
-        },
-        # options = {
-        #     "sort": "oldest",
-        #     "sources": [
-        #         "New York Times",
-        #         "AP",
-        #         "Reuters",
-        #         "International Herald Tribune"
-        #     ],
-        #     "news_desk": [
-        #         "Politics"
-        #     ],
-        #     "type_of_material": [
-        #         "News Analysis"
-        #     ]
-        # }
-    )
-    for art in articles:
+    """
+    Function that includes the query search. Includes 3 parameters: query, start date, and end date 
+    """
+    a, b, c = begin.split(',')  
+    x, y, z = end.split(',')  
+    start_date = datetime.datetime(int(a), int(b), int(c))  
+    end_date = datetime.datetime(int(x), int(y), int(z))
+    all_data = []  
+    for page in range(100):  
+        try:  
+            articles: list = nyt.article_search(  
+                query=query,
+                results=99,
+                dates={
+                    "begin": start_date,
+                    "end": end_date
+                },
+                options={
+                    'page': str(page)
+                }
+            )
+            if articles:  
+                print("page : ", page, "content : ", len(articles))
+                all_data.extend(articles)  
+                time.sleep(6)  
+            else:
+                break
+        except requests.exceptions.HTTPError:  
+            continue
+    print("Scraping...")
+    for art in all_data:  
         cont = content(art['web_url'])
-        art['content'] = cont
+        art['content'] = cont  
         print(art['web_url'])
         time.sleep(1)
-    return articles
+    return all_data
 
 
 if __name__ == '__main__':
-    while True:
-        word = input('\nPlease enter search term: ')
-        begin = input('\nStarting date(i.e:2019,3,15): ')
-        end = input('\nEnd date(i.e:2019,3,15): ')
-        articles = search(word, begin, end)
-        if articles:
-            df = pd.DataFrame(articles)
-            filename = word.split()[0] + ' ' + str(datetime.datetime.now().date()) + '.xlsx'
-            df.to_excel(filename)
-            print(f'\nResults were saved under {filename}.')
-        else:
-            print('\nNo results were found...\n')
+    # while True:
+    word = input('\nPlease enter search term: ')
+    begin = input('\nStart date(i.e:2019,3,15): ')
+    end = input('\nEnd date(i.e:2019,3,15): ')
+    articles = search(word, begin, end)
+    if articles:
+        df = pd.DataFrame(articles)  
+        filename = word.split()[0] + ' ' + str(datetime.datetime.now().date()) + '.csv'
+        df.to_csv(filename)  
+        print(f'\nResults were saved under {filename}.')
+    else:
+        print('\nNo results were found...\n')
