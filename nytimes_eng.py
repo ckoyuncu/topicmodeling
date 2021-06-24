c6414ab46e562e756c00c546ced6a464d3ac45fc
ckoyuncu <59180341+ckoyuncu@users.noreply.github.com>
2021-06-24 16:40:42 +0200

    Update nytimes_eng.py

--- nytimes_eng.py
+++ nytimes_eng.py
@@ -8,8 +8,7 @@ import time
 
 API_KEY = '1HXamLhbfLrsj4BUYmKZUGnZV2QLwAtR'
 
-nyt = NYTAPI(API_KEY, https=False)
-
+nyt = NYTAPI(API_KEY, https=False)  
 
 def content(url):
     """
@@ -18,7 +17,7 @@ def content(url):
     try:
         result = ''  
         site = urllib.request.urlopen(url)  
-        content = site.read()
+        content = site.read()  
         soup = BeautifulSoup(content, 'lxml')  
         table = soup.findAll('p')  
         for line in table: 
@@ -31,40 +30,41 @@ def content(url):
 
 def search(query, begin, end):
     """
-    Function that includes the query search. Includes 3 parameters: query, start date, and end date 
+    Function that includes the query search. Includes 3 parameters: query, start date, and end date
     """
     a, b, c = begin.split(',')  
     x, y, z = end.split(',')  
     start_date = datetime.datetime(int(a), int(b), int(c))  
     end_date = datetime.datetime(int(x), int(y), int(z))
+    dayrange = range((end_date - start_date).days + 1)
     all_data = []  
-    for page in range(100):  
+    for daycount in dayrange:
+        dt = start_date + datetime.timedelta(days=daycount)
+        #datestr = dt.strftime('%Y-%m-%d')
+        print("Downloading", str(dt))
         try:  
             articles: list = nyt.article_search(  
                 query=query,
                 results=99,
                 dates={
-                    "begin": start_date,
-                    "end": end_date
+                    "begin": dt,
+                    "end": dt
                 },
-                options={
-                    'page': str(page)
-                }
+                #options={'page': str(page)}
             )
             if articles:  
-                print("page : ", page, "content : ", len(articles))
+                print("content : ", len(articles))
+                for art in articles:  
+                    cont = content(art['web_url'])
+                    art['content'] = cont  
+                    print(art['web_url'])
+                    time.sleep(1)
                 all_data.extend(articles)  
                 time.sleep(6)  
-            else:
-                break
         except requests.exceptions.HTTPError:  
-            continue
+            pass
     print("Scraping...")
-    for art in all_data:  
-        cont = content(art['web_url'])
-        art['content'] = cont  
-        print(art['web_url'])
-        time.sleep(1)
+    
     return all_data
 
 
@@ -76,8 +76,8 @@ if __name__ == '__main__':
     articles = search(word, begin, end)
     if articles:
         df = pd.DataFrame(articles)  
-        filename = word.split()[0] + ' ' + str(datetime.datetime.now().date()) + '.csv'
-        df.to_csv(filename)  
+        filename = word.split()[0] + ' ' + str(datetime.datetime.now().date()) + '..csv'
+        df.to_excel(filename)  
         print(f'\nResults were saved under {filename}.')
     else:
         print('\nNo results were found...\n')
